98f8922fbe3d74c2f3317d11bf5198ab4c450efb
Andre Borges Ribeiro <56261783+andre88br@users.noreply.github.com>
2026-08-17 05:39:23 -0300

    Add offset and size imports to CardTable.kt

--- app/src/main/kotlin/io/github/andre88br/newgame/app/ui/board/surfaces/CardTable.kt
+++ app/src/main/kotlin/io/github/andre88br/newgame/app/ui/board/surfaces/CardTable.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
